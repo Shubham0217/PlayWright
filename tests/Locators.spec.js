1db687ddbe36b9b1d4ab4fc6a8a96fbc21c5d1ec
Shubham0217 <shubham0127singh@gmail.com>
2025-06-05 19:39:25 +0530

    Locators

---
+++ tests/Locators.spec.js
@@ -0,0 +1,38 @@
+import {test, expect} from '@playwright/test';
+
+
+
+test.describe('verify Title', ()=> {
+    test('verify Title', async({page})=>{
+        
+        await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        await expect(page).toHaveTitle('OrangeHRM');
+        await page.waitForTimeout(2000);
+    });
+});
+
+
+
+test.describe('Locators', ()=>{
+
+    test('LoginToOrangeHrm', async({page})=>{
+    await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+
+    // await page.locator('id').click(); two types
+
+    await page.fill('input[name=username]', 'Admin');
+
+    await page.waitForTimeout(2000);
+
+    await page.fill('input[name=password]', 'admin123');
+
+    await page.waitForTimeout(2000);
+
+    await page.click('button[type=submit]');
+
+    await page.waitForTimeout(2000);
+});
+
+
+
+});
