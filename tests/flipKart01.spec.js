510e5b50e2374da258ae0522ae611c7708f671a9
Shubham0217 <shubham0127singh@gmail.com>
2025-06-28 23:57:49 +0530

    newadded

--- tests/flipKart01.spec.js
+++ tests/flipKart01.spec.js
@@ -3,6 +3,7 @@ import { test, expect } from '@playwright/test';
 
 test.describe('Flipkart', () => {
 
+
     test('VerifyTitle', async ({ page }) => {
         await page.goto("https://www.flipkart.com/");
         await expect(page).toHaveTitle('Online Shopping Site for Mobiles, Electronics, Furniture, Grocery, Lifestyle, Books & More. Best Offers!');
@@ -11,8 +12,11 @@ test.describe('Flipkart', () => {
 
     test('LoginFlipkart', async ({ page }) => {
         await page.goto("https://www.flipkart.com/");
+        
         await page.waitForSelector("//span[text() = 'Login']", { timeout: 5000 });
+        
         await page.click("//span[text() = 'Login']");
+
         await page.waitForTimeout(200);
 
         await page.fill("//input[@class='r4vIwl BV+Dqf']",  "7509474508");
